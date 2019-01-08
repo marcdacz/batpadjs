38f555d5fad863c8cd96912298543d2f2b224c33
Marc Dacanay <mdacanay@gmail.com>
2019-01-08 21:10:06 +1100

    Implement draft test runner

--- cli/index.js
+++ cli/index.js
@@ -1,2 +1,7 @@
 #!/usr/bin/env node
 console.log(`Oh hey guys, its me BatPad... cool!!!`);
+
+const testRunner = require("../core/util/runTest");
+
+const testSuite = require("../batpad");
+testRunner.runTests(testSuite);
--- core/util/runTest.js
+++ core/util/runTest.js
@@ -1,22 +1,38 @@
-module.exports = async (args) => {
+const axios = require("axios");
 
-};
-
-
-const executeScenario = async (scenario, config) => {
+module.exports.runTests = async testSuite => {
+  const scenarios = testSuite.scenarios;
+  const configs = testSuite.configs;
 
   // --- BEFORE SCRIPT ---
+  // await require(config.beforeAllScript)();
 
-
-  // --- SEND REQUEST ---
-
-
-
-
-  // -- ASSERT RESPONSE ---
-
-
+  // --- EXECUTE SCENARIO ---
+  scenarios.map(async scenario => {
+    await executeScenario(scenario, configs);
+  });
 
   // --- AFTER SCRIPT ---
+  // await require(config.afterAllScript)();
+};
 
+const executeScenario = async (scenario, configs) => {
+  // --- BEFORE SCRIPT ---
+  // --- SEND REQUEST ---
+  console.log("YEY Test!");
+  // console.log(JSON.stringify(scenario));
+
+  let res = await axios({
+    method: configs.defaultMethod,
+    url: configs.baseUrl + configs.defaultEndpoint,
+    data: {
+      id: 12345,
+      title: "Flintstone",
+      body: "Blah blah blah"
+    }
+  }); //.then(res => console.log(res.data));
+  console.log(res.data);
+
+  // -- ASSERT RESPONSE ---
+  // --- AFTER SCRIPT ---
 };
