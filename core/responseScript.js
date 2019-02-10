d81db5e6dbb62e3edb3ba080deb89930fdff3e95
Marc Dacanay <mdacanay@gmail.com>
2019-02-10 22:24:17 +1100

    Allow AfterScript access to Actual Response

--- core/responseScript.js
+++ core/responseScript.js
@@ -1,65 +1,59 @@
 const jsonpath = require("jsonpath");
-const log = require("./logger");
-let isFailed;
 
-module.exports = async (scenario, actualResponse, configs) => {  
-  scenario.result.state = 'passed';  
-  isFailed = false;
-  
-  if (scenario.expected && actualResponse) {
-    let expectedResponse = scenario.expected;   
+module.exports = async (scenario, actualResponse, configs) => { 
+	scenario.result.state = 'passed'; 
 
-    if (expectedResponse.status) {
-      if (actualResponse.status != expectedResponse.status) {
-        isFailed = true;
-        scenario.result.context.push({
-          error: "Response status is incorrect!",
-          actual: actualResponse.status,
-          expected: expectedResponse.status
-        });
-      }
-    }
+	if (scenario.expected && actualResponse) {
+		let expectedResponse = scenario.expected; 
 
-    if (expectedResponse.statusText) {
-      if (actualResponse.statusText != expectedResponse.statusText) {
-        isFailed = true;
-        scenario.result.context.push({
-          error: "Response status text is incorrect!",
-          actual: actualResponse.statusText,
-          expected: expectedResponse.statusText
-        });
-      }
-    }
+		if (expectedResponse.status) {
+			if (actualResponse.status != expectedResponse.status) {
+				scenario.result.state = 'failed';
+				scenario.result.context.push({
+					error: "Response status is incorrect!",
+					actual: actualResponse.status,
+					expected: expectedResponse.status
+				});
+			}
+		}
 
-    if (expectedResponse.data) {
-      for (const dataField of expectedResponse.data) {
-        if (actualResponse.data) {          
-          let actualValue = jsonpath.value(actualResponse.data, dataField.path);
-          let expectedValue = dataField.value;
-          if (actualValue != expectedValue) {
-            isFailed = true;
-            scenario.result.context.push({
-              error: "Field value is incorrect!",
-              path: dataField.path,
-              actual: actualValue,
-              expected: expectedValue
-            });
-          }
-        } else {
-          isFailed = true;
-          scenario.result.context.push({
-            error: "Field not found!"
-          });
-        }        
-      }
-    }
-  } else {
-    isFailed = true;
-    scenario.result.context.push({
-      error: `Response is undefined!`
-    });
-  }
+		if (expectedResponse.statusText) {
+			if (actualResponse.statusText != expectedResponse.statusText) {
+				scenario.result.state = 'failed';
+				scenario.result.context.push({
+					error: "Response status text is incorrect!",
+					actual: actualResponse.statusText,
+					expected: expectedResponse.statusText
+				});
+			}
+		}
 
-  
-  return isFailed;
+		if (expectedResponse.data) {
+			for (const dataField of expectedResponse.data) {
+				if (actualResponse.data) { 
+					let actualValue = jsonpath.value(actualResponse.data, dataField.path);
+					let expectedValue = dataField.value;
+					if (actualValue != expectedValue) {
+						scenario.result.state = 'failed';
+						scenario.result.context.push({
+							error: "Field value is incorrect!",
+							path: dataField.path,
+							actual: actualValue,
+							expected: expectedValue
+						});
+					}
+				} else {
+				    scenario.result.state = 'failed';
+					  scenario.result.context.push({
+						error: "Field not found!"
+					});
+				} 
+			}
+		}
+	} else {
+    scenario.result.state = 'failed';
+		scenario.result.context.push({
+			error: `Response is undefined!`
+		});
+	}
 };
--- core/testRunner.js
+++ core/testRunner.js
@@ -16,165 +16,165 @@ const DEFAULT_DELAY = 0;
 const DEFAULT_ASYNC_LIMIT = 1;
 
 module.exports.runTests = async (opts) => {
-  opts = opts ? opts : {};
-  let testFilter = opts.filter || DEFAULT_TESTS_FILTER;
-  let settingsFile = DEFAULT_SETTINGS_FILE;
-  let settings = fileHelpers.requireUncached(settingsFile);
-  let testSuitesPath = settings.paths.tests || DEFAULT_TESTS_PATH;
-  let testSuites = fileHelpers.getJsFiles(testSuitesPath);
-  let defaultGlobalDelay = settings.delay || DEFAULT_DELAY;
-  let defaultGlobalAsyncLimit = settings.asyncLimit || DEFAULT_ASYNC_LIMIT;
+	opts = opts ? opts : {};
+	let testFilter = opts.filter || DEFAULT_TESTS_FILTER;
+	let settingsFile = DEFAULT_SETTINGS_FILE;
+	let settings = fileHelpers.requireUncached(settingsFile);
+	let testSuitesPath = settings.paths.tests || DEFAULT_TESTS_PATH;
+	let testSuites = fileHelpers.getJsFiles(testSuitesPath);
+	let defaultGlobalDelay = settings.delay || DEFAULT_DELAY;
+	let defaultGlobalAsyncLimit = settings.asyncLimit || DEFAULT_ASYNC_LIMIT;
 
-  let reporter = new Reporter();
+	let reporter = new Reporter();
 
-  await Promise.map(
-    testSuites,
-    testSuite => {
-      let suite = fileHelpers.requireUncached(testSuite);
-      if (suite.configs && suite.scenarios) {
-        return new Promise(function(resolve, reject) {
-          setTimeout(() =>
-            resolve(executeSuite(suite, testFilter, reporter)), defaultGlobalDelay);
-        });
-      }
-    },
-    { concurrency: defaultGlobalAsyncLimit }
-  );
+	await Promise.map(
+		testSuites,
+		testSuite => {
+			let suite = fileHelpers.requireUncached(testSuite);
+			if (suite.configs && suite.scenarios) {
+				return new Promise(function(resolve, reject) {
+					setTimeout(() =>
+						resolve(executeSuite(suite, testFilter, reporter)), defaultGlobalDelay);
+				});
+			}
+		},
+		{ concurrency: defaultGlobalAsyncLimit }
+	);
 
-  // --- SAVE TEST REPORT ---  
-  reporter.saveTestRunReport();
-  displayOverallTestResult(reporter);
+	// --- SAVE TEST REPORT --- 
+	reporter.saveTestRunReport();
+	displayOverallTestResult(reporter);
 };
 
 const displayOverallTestResult = (reporter) => {
-  log.lines();
-  log.info('------------------------------------------------------------------------------');
-  log.keyValue(`Start:`,            `\t\t\t${reporter.test.result.start}`);
-  log.keyValue(`End:`,              `\t\t\t${reporter.test.result.end}`);
-  log.keyValue(`Duration:`,         `\t\t${reporter.test.result.duration}`);
-  log.keyValue(`Total:`,            `\t\t\t${reporter.test.result.totalTestCount}`);
-  log.keyValue(`Passed:`,           `\t\t${reporter.test.result.totalPassedTestCount}`);
-  log.keyValue(`Failed:`,           `\t\t${reporter.test.result.totalFailedTestCount}`);
-  log.keyValue(`Pass Percentage:`,  `\t${reporter.test.result.passPercentage}`);
-  log.info('------------------------------------------------------------------------------');
+	log.lines();
+	log.info('------------------------------------------------------------------------------');
+	log.keyValue(`Start:`,            `\t\t\t${reporter.test.result.start}`);
+	log.keyValue(`End:`,              `\t\t\t${reporter.test.result.end}`);
+	log.keyValue(`Duration:`,         `\t\t${reporter.test.result.duration}`);
+	log.keyValue(`Total:`,            `\t\t\t${reporter.test.result.totalTestCount}`);
+	log.keyValue(`Passed:`,           `\t\t${reporter.test.result.totalPassedTestCount}`);
+	log.keyValue(`Failed:`,           `\t\t${reporter.test.result.totalFailedTestCount}`);
+	log.keyValue(`Pass Percentage:`,  `\t${reporter.test.result.passPercentage}`);
+	log.info('------------------------------------------------------------------------------');
 
-  log.lines();
-  if (reporter.test.result.state === 'passed') {    
-    log.info('TEST RUN SUCCESSULLY FINISHED! \uD83D\uDE0E')
-  } else if (reporter.test.result.state === 'failed') {
-    log.error('TEST RUN FAILED! \uD83D\uDE22');
-    let count = 1;
-    let failedTests = reporter.getFailedTests();
-    failedTests.map(scenario => {
-      log.error(`\n${count}. ${scenario.test}`);
-      log.warn(`Test Context:`);
-      log.failedTestContext(scenario.result.context);
-      count++;
-    });
+	log.lines();
+	if (reporter.test.result.state === 'passed') { 
+		log.info('TEST RUN SUCCESSULLY FINISHED! \uD83D\uDE0E')
+	} else if (reporter.test.result.state === 'failed') {
+		log.error('TEST RUN FAILED! \uD83D\uDE22');
+		let count = 1;
+		let failedTests = reporter.getFailedTests();
+		failedTests.map(scenario => {
+			log.error(`\n${count}. ${scenario.test}`);
+			log.warn(`Test Context:`);
+			log.failedTestContext(scenario.result.context);
+			count++;
+		});
   }
+  log.lines();
 }
 
 const runScript = async (scriptPath, testObject) => {
-  if (scriptPath) {
-    let scriptResolvedPath = path.resolve(scriptPath);
-    if (fs.existsSync(scriptResolvedPath)) {
-      try {
-        await require(scriptResolvedPath)(testObject);
-      } catch (error) {
-        log.error(`ERROR: ${error}`);
-      }
-    } else {
-      log.warn(`WARNING: Script not found: ${scriptPath}`);
-    }
-  }
+	if (scriptPath) {
+		let scriptResolvedPath = path.resolve(scriptPath);
+		if (fs.existsSync(scriptResolvedPath)) {
+			try {
+				await require(scriptResolvedPath)(testObject);
+			} catch (error) {
+				log.error(`ERROR: ${error}`);
+			}
+		} else {
+			log.warn(`WARNING: Script not found: ${scriptPath}`);
+		}
+	}
 };
 
 const executeSuite = async (testSuite, testFilter, reporter) => {
-  const configs = testSuite.configs;  
-  const scenarios = testSuite.scenarios.filter(scenario => scenario.test.match(testFilter));
+	const configs = testSuite.configs; 
+	const scenarios = testSuite.scenarios.filter(scenario => scenario.test.match(testFilter));
 
-  let defaultDelay = configs.delay || DEFAULT_DELAY;
-  let defaultAsyncLimit = configs.asyncLimit || DEFAULT_ASYNC_LIMIT;
+	let defaultDelay = configs.delay || DEFAULT_DELAY;
+	let defaultAsyncLimit = configs.asyncLimit || DEFAULT_ASYNC_LIMIT;
 
-  if (scenarios.length > 0) {
-    log.info(testSuite.name);
+	if (scenarios.length > 0) {
+		log.info(testSuite.name);
 
-    // --- BEFORE ALL SCRIPT ---
-    await runScript(configs.beforeAllScript, configs);
+		// --- BEFORE ALL SCRIPT ---
+		await runScript(configs.beforeAllScript, configs);
 
-    // --- EXECUTE SCENARIO ---
-    await Promise.map(
-      scenarios,
-      scenario => {
-        return new Promise(function(resolve, reject) {
-          setTimeout(() => resolve(executeScenario(scenario, configs, reporter)), defaultDelay);
-        });
-      },
-      { concurrency: defaultAsyncLimit }
-    );
+		// --- EXECUTE SCENARIO ---
+		await Promise.map(
+			scenarios,
+			scenario => {
+				return new Promise(function(resolve, reject) {
+					setTimeout(() => resolve(executeScenario(scenario, configs, reporter)), defaultDelay);
+				});
+			},
+			{ concurrency: defaultAsyncLimit }
+		);
 
-    // --- AFTER ALL SCRIPT ---
-    await runScript(configs.afterAllScript, configs);
+		// --- AFTER ALL SCRIPT ---
+		await runScript(configs.afterAllScript, configs);
 
-    // --- TEST REPORT ---
-    reporter.addTest(testSuite);
-  }
+		// --- TEST REPORT ---
+		reporter.addTest(testSuite);
+	}
 }
 
 const executeScenario = async (scenario, configs, reporter) => {
-  scenario.request = scenario.request ? scenario.request : {};
-  scenario.request.fields = scenario.request.fields ? scenario.request.fields : [];
-  scenario.result = {};
-  scenario.result.context = [];
-  scenario.result.state = 'pending';
-  scenario.result.start = moment();
+	scenario.request = scenario.request ? scenario.request : {};
+	scenario.request.fields = scenario.request.fields ? scenario.request.fields : [];
+	scenario.result = {};
+	scenario.result.context = [];
+	scenario.result.state = 'pending';
+	scenario.result.start = moment();
 
-  // --- BEFORE SCRIPT ---
-  await runScript(configs.beforeEachScript, scenario);
-  await runScript(scenario.beforeScript, scenario);
+	// --- BEFORE SCRIPT ---
+	await runScript(configs.beforeEachScript, scenario);
+	await runScript(scenario.beforeScript, scenario);
 
-  // --- REQUEST SCRIPT ---
-  require('./requestScript')(scenario, configs);
+	// --- REQUEST SCRIPT ---
+	require('./requestScript')(scenario, configs);
 
-  // --- SEND REQUEST ---
-  let actualResponse;
-  try {
-    let url = scenario.request.url ? configs.baseUrl + scenario.request.url : configs.baseUrl + configs.defaultEndpoint
-    const res = await axios({
-      url: url,
-      method: scenario.request.method || configs.defaultMethod || DEFAULT_METHOD,
-      headers: scenario.headers,
-      params: scenario.params,
-      data: scenario.request.body,
-      timeout: configs.timeout,
-      withCredentials: scenario.withCredentials,
-      auth: scenario.auth,
-      xsrfCookieName: scenario.xsrfCookieName,
-      xsrfHeaderName: scenario.xsrfHeaderName,
-      proxy: scenario.proxy
-    });
-    actualResponse = res;
-  } catch (error) {
-    if (error.response) {
-      actualResponse = error.response;
-    }
-  }
+	// --- SEND REQUEST ---
+	let actualResponse;
+	try {
+		let url = scenario.request.url ? configs.baseUrl + scenario.request.url : configs.baseUrl + configs.defaultEndpoint
+		const res = await axios({
+			url: url,
+			method: scenario.request.method || configs.defaultMethod || DEFAULT_METHOD,
+			headers: scenario.headers,
+			params: scenario.params,
+			data: scenario.request.body,
+			timeout: configs.timeout,
+			withCredentials: scenario.withCredentials,
+			auth: scenario.auth,
+			xsrfCookieName: scenario.xsrfCookieName,
+			xsrfHeaderName: scenario.xsrfHeaderName,
+			proxy: scenario.proxy
+		});
+		actualResponse = res;
+	} catch (error) {
+		if (error.response) {
+			actualResponse = error.response;
+		}
+	}
 
-  // --- RESPONSE SCRIPT ---
-  let isFailed = await require('./responseScript')(scenario, actualResponse, configs);
+	// --- RESPONSE SCRIPT ---
+	await require('./responseScript')(scenario, actualResponse, configs);
 
-  // --- AFTER SCRIPT ---
-  await runScript(scenario.afterScript, scenario);
-  await runScript(configs.afterEachScript, scenario);
+	// --- AFTER SCRIPT ---
+	await runScript(scenario.afterScript, { scenario: scenario, actualResponse: actualResponse });
+	await runScript(configs.afterEachScript, { scenario: scenario, actualResponse: actualResponse });
 
-  scenario.result.end = moment();
-  scenario.result.duration = timeHelpers.getDuration(scenario.result.start, scenario.result.end);
-  if (isFailed) {
-    reporter.setTestRunResult('failed');
-    scenario.result.state = 'failed';
-    log.failedTestContext(scenario.result.context);
-    log.failedTest(scenario.test, scenario.result.duration);
-  } else {
-    log.passedTest(scenario.test, scenario.result.duration);
-  }
+	scenario.result.end = moment();
+	scenario.result.duration = timeHelpers.getDuration(scenario.result.start, scenario.result.end);
+	if (scenario.result.state === 'failed') {
+		reporter.setTestRunResult('failed');
+		log.failedTestContext(scenario.result.context);
+		log.failedTest(scenario.test, scenario.result.duration);
+	} else {
+		log.passedTest(scenario.test, scenario.result.duration);
+	}
 };
