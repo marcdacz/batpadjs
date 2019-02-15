4fb1d21b22edaf18abe34f76c57f4604e4e681f8
Marc Dacanay <mdacanay@gmail.com>
2019-02-15 21:51:22 +1100

    Add Equals, Contains and NotContains Response Data Validation

--- core/responseScript.js
+++ core/responseScript.js
@@ -1,59 +1,101 @@
 const jsonpath = require("jsonpath");
 
-module.exports = async (scenario, actualResponse, configs) => { 
-	scenario.result.state = 'passed'; 
+module.exports = async (scenario, actualResponse, configs) => {
 
-	if (scenario.expected && actualResponse) {
-		let expectedResponse = scenario.expected; 
+  if (scenario.expected && actualResponse) {
+    let expectedResponse = scenario.expected;
 
-		if (expectedResponse.status) {
-			if (actualResponse.status != expectedResponse.status) {
-				scenario.result.state = 'failed';
-				scenario.result.context.push({
-					error: "Response status is incorrect!",
-					actual: actualResponse.status,
-					expected: expectedResponse.status
-				});
-			}
-		}
+    if (expectedResponse.status) {
+      if (actualResponse.status != expectedResponse.status) {
+        scenario.result.state = 'failed';
+        scenario.result.context.push({
+          error: "Response status is incorrect!",
+          actual: actualResponse.status,
+          expected: expectedResponse.status
+        });
+      } else {
+        scenario.result.state = 'passed';
+      }
+    }
 
-		if (expectedResponse.statusText) {
-			if (actualResponse.statusText != expectedResponse.statusText) {
-				scenario.result.state = 'failed';
-				scenario.result.context.push({
-					error: "Response status text is incorrect!",
-					actual: actualResponse.statusText,
-					expected: expectedResponse.statusText
-				});
-			}
-		}
+    if (expectedResponse.statusText) {
+      if (actualResponse.statusText != expectedResponse.statusText) {
+        scenario.result.state = 'failed';
+        scenario.result.context.push({
+          error: "Response status text is incorrect!",
+          actual: actualResponse.statusText,
+          expected: expectedResponse.statusText
+        });
+      } else {
+        scenario.result.state = 'passed';
+      }
+    }
 
-		if (expectedResponse.data) {
-			for (const dataField of expectedResponse.data) {
-				if (actualResponse.data) { 
-					let actualValue = jsonpath.value(actualResponse.data, dataField.path);
-					let expectedValue = dataField.value;
-					if (actualValue != expectedValue) {
-						scenario.result.state = 'failed';
-						scenario.result.context.push({
-							error: "Field value is incorrect!",
-							path: dataField.path,
-							actual: actualValue,
-							expected: expectedValue
-						});
-					}
-				} else {
-				    scenario.result.state = 'failed';
-					  scenario.result.context.push({
-						error: "Field not found!"
-					});
-				} 
-			}
-		}
-	} else {
+    if (expectedResponse.data) {
+      for (const dataField of expectedResponse.data) {
+        if (actualResponse.data) {
+          let actualValue = jsonpath.value(actualResponse.data, dataField.path);
+
+          // Equals
+          if (dataField.equals) {
+            let expectedValue = dataField.equals;
+            if (actualValue != expectedValue) {
+              scenario.result.state = 'failed';
+              scenario.result.context.push({
+                error: "Field value is incorrect!",
+                path: dataField.path,
+                actual: actualValue,
+                expected: expectedValue
+              });
+            } else {
+              scenario.result.state = 'passed';
+            }
+          }
+
+          // Contains
+          if (dataField.contains) {
+            let regex = RegExp(dataField.contains);
+            if (regex.test(actualValue)) {
+              scenario.result.state = 'passed';              
+            } else {
+              scenario.result.state = 'failed';
+              scenario.result.context.push({
+                error: "Field value is incorrect!",
+                path: dataField.path,
+                actual: actualValue,
+                contains: dataField.contains
+              });
+            }
+          }
+
+          // NotContains
+          if (dataField.notcontains) {
+            let regex = RegExp(dataField.notcontains);
+            if (!regex.test(actualValue)) {
+              scenario.result.state = 'passed';              
+            } else {
+              scenario.result.state = 'failed';
+              scenario.result.context.push({
+                error: "Field value is incorrect!",
+                path: dataField.path,
+                actual: actualValue,
+                notcontains: dataField.notcontains
+              });
+            }
+          }
+        } else {
+          scenario.result.state = 'failed';
+          scenario.result.context.push({
+            error: "Field not found!"
+          });
+        }
+      }
+    }
+
+  } else {
     scenario.result.state = 'failed';
-		scenario.result.context.push({
-			error: `Response is undefined!`
-		});
-	}
+    scenario.result.context.push({
+      error: `Response is undefined!`
+    });
+  }
 };
--- scripts/beforeAllScript.js
+++ scripts/beforeAllScript.js
@@ -1,3 +1,4 @@
-module.exports = async (configs) => {  
-  configs.defaultBody.title = "Using Default Body from Configs updated in BeforeAll script";
+module.exports = async (configs) => {
+  configs.defaultBody.title = "Using Default Body Title from Configs updated in BeforeAll script";
+  configs.defaultBody.body = "Using Default Body Data from Configs updated in BeforeAll script";
 };
